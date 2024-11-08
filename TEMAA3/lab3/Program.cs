9153d04b4ac2d0d6242bddc622027d6f37a99906
Alex Lupu <alex203lupu@gmail.com>
2024-11-08 20:12:57 +0200

    tema 03 & 04 & 05

---
+++ TEMAA3/lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (ImmediateMode example = new ImmediateMode()) 
+            {
+                example.Run(30.0, 0.0);// se activeaza activeaza bucla de control 
+            }
+        }
+    }
+}
